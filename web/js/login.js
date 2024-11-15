1d5f1934b11619b8d5c04ba20b31a145fd4d7ebf
IsaacDev2024 <158796340+IsaacDev2024@users.noreply.github.com>
2024-11-14 23:13:09 -0500

    Add files via upload

--- web/js/login.js
+++ web/js/login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Función para manejar el login
     document.getElementById('login-form')?.addEventListener('submit', async (event) => {
         event.preventDefault();
-        const email = document.getElementById('email').value;
+        const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
         try {
@@ -13,27 +13,36 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify({ username, password }),
             });
 
             if (!response.ok) throw new Error('Error en el inicio de sesión. Verifica tus credenciales.');
 
             const data = await response.json();
+
             // Guarda el token de autenticación si se provee
             localStorage.setItem('authToken', data.token);
-            localStorage.setItem('full_name', data.full_name);
-            alert('¡Inicio de sesión exitoso!');
-            window.location.href = '/';
+            localStorage.setItem('user_id', data.user_id);
+
+            createSuccessModal('¡Inicio de sesión exitoso! Redirigiendo en breves'); // Modal de éxito
+
+            // Redirige después de 4 segundos
+            setTimeout(() => {
+                // Redirige a la página original
+                const urlParams = new URLSearchParams(window.location.search);
+                const redirectUrl = urlParams.get('redirect') || '/';
+                window.location.href = redirectUrl;
+            }, 4000);
+
         } catch (error) {
             console.error('Error en el inicio de sesión:', error);
-            alert('Credenciales incorrectas. Por favor, verifica y vuelve a intentar.');
+            createErrorModal('Credenciales incorrectas', 'Por favor, verifica y vuelve a intentar.'); // Modal de error
         }
     });
 });
 
-function validateEmail(email) {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
+function validateUsername(username) {
+    return username.length >= 3;
 }
 
 function validatePassword(password) {
@@ -43,12 +52,12 @@ function validatePassword(password) {
 document.addEventListener('DOMContentLoaded', () => {
     // Validación para el login
     document.getElementById('login-form')?.addEventListener('input', () => {
-        const email = document.getElementById('email').value;
+        const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const submitButton = document.querySelector('.btn');
 
         // Validar correo y contraseña
-        if (validateEmail(email) && validatePassword(password)) {
+        if (validateUsername(username) && validatePassword(password)) {
             submitButton.disabled = false;
         } else {
             submitButton.disabled = true;
@@ -56,3 +65,64 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Modales de éxito y error
+function createSuccessModal(message) {
+    const modalHtml = `
+        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="successModalLabel"><i class="fas fa-check-circle"></i> Acción Exitosa</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>${message}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
+
+    // Usar el método modal de Bootstrap 4 para mostrar el modal
+    $('#successModal').modal('show');
+
+    // Remover el modal después de que se cierre
+    $('#successModal').on('hidden.bs.modal', function () {
+        $(this).remove();
+    });
+}
+
+function createErrorModal(title, errorMessage, helpLink = '') {
+    const modalHtml = `
+        <div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="errorModalLabel"><i class="fas fa-exclamation-circle"></i> ${title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="text-align: justify;">
+                        <p><i class="fas fa-info-circle"></i> ${errorMessage}</p>
+                        ${helpLink ? `<p style="text-align: center;"><a href="${helpLink}" target="_blank">Obtener más información</a></p>` : ''}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
+
+    // Usar el método modal de Bootstrap 4 para mostrar el modal
+    $('#errorModal').modal('show');
+
+    // Remover el modal después de que se cierre
+    $('#errorModal').on('hidden.bs.modal', function () {
+        $(this).remove();
+    });
+}
--- web/js/register.js
+++ web/js/register.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const termsCheckbox = document.querySelector('input[type="checkbox"]');
         if (!termsCheckbox.checked) {
-            alert('Debes aceptar los términos y condiciones.');
+            createErrorModal('Error', 'Debes aceptar los términos y condiciones.');
             return;
         }
 
@@ -15,6 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const full_name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
+
+        // Validar contraseña
+        const passwordValidation = validatePassword(password);
+        if (!passwordValidation.isValid) {
+            createErrorModal('Error en la contraseña', passwordValidation.messages.join('<br>'));
+            return;
+        }
 
         try {
             const response = await fetch(`${API_BASE_URL}/users`, {
@@ -27,21 +34,118 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (!response.ok) throw new Error('Error en el registro. Intenta nuevamente.');
 
-            alert('¡Registro completado con éxito! Inice sesión para confirmar.');
-            window.location.href = 'login.html';
+            createSuccessModal('¡Registro completado con éxito! Inicia sesión para confirmar. Será redirigido en 5 segundos.');
+
+            // Redirigir después de 5 segundos
+            setTimeout(() => {
+                window.location.href = 'login.html'; 
+            }, 5000); // 5000 ms = 5 segundos
         } catch (error) {
             console.error('Error en el registro:', error);
-            alert('Hubo un problema al registrar. Por favor, verifica tus datos.');
+            createErrorModal('Error', 'Hubo un problema al registrar. Por favor, verifica tus datos.');
         }
     });
 });
 
-function validateEmail(email) {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-}
-
+// Función para validar la contraseña
 function validatePassword(password) {
-    return password.length >= 6;
+    const minLength = 8;
+    const errors = [];
+
+    // Verificar la longitud mínima
+    if (password.length < minLength) {
+        errors.push('La contraseña debe tener al menos 8 caracteres.');
+    }
+    // Verificar la presencia de mayúsculas
+    if (!/[A-Z]/.test(password)) {
+        errors.push('La contraseña debe incluir al menos una letra mayúscula.');
+    }
+    // Verificar la presencia de minúsculas
+    if (!/[a-z]/.test(password)) {
+        errors.push('La contraseña debe incluir al menos una letra minúscula.');
+    }
+    // Verificar la presencia de números
+    if (!/[0-9]/.test(password)) {
+        errors.push('La contraseña debe incluir al menos un número.');
+    }
+    // Verificar la presencia de símbolos especiales
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        errors.push('La contraseña debe incluir al menos un símbolo especial.');
+    }
+
+    // Si hay errores, devolvemos los errores encontrados
+    if (errors.length > 0) {
+        return {
+            isValid: false,
+            messages: errors // Devolvemos un array de errores
+        };
+    }
+
+    // Si la contraseña es válida
+    return {
+        isValid: true,
+        messages: []
+    };
 }
 
+// Modales de éxito y error
+function createSuccessModal(message) {
+    const modalHtml = `
+        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="successModalLabel"><i class="fas fa-check-circle"></i> Acción Exitosa</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>${message}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
+
+    // Usar el método modal de Bootstrap 4 para mostrar el modal
+    $('#successModal').modal('show');
+
+    // Remover el modal después de que se cierre
+    $('#successModal').on('hidden.bs.modal', function () {
+        $(this).remove();
+    });
+}
+
+function createErrorModal(title, errorMessage, helpLink = '') {
+    const modalHtml = `
+        <div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="errorModalLabel"><i class="fas fa-exclamation-circle"></i> ${title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="text-align: justify;">
+                        <p><i class="fas fa-info-circle"></i> ${errorMessage}</p>
+                        ${helpLink ? `<p style="text-align: center;"><a href="${helpLink}" target="_blank">Obtener más información</a></p>` : ''}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
+
+    // Usar el método modal de Bootstrap 4 para mostrar el modal
+    $('#errorModal').modal('show');
+
+    // Remover el modal después de que se cierre
+    $('#errorModal').on('hidden.bs.modal', function () {
+        $(this).remove();
+    });
+}
